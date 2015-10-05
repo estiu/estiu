7de0b27d722a2fa8dc812061f4717a942843f6b9
vemv <vemv@vemv.net>
2015-10-05 20:15:29 +0200

    Create and use mechanism for abstracting form field types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
     }[flash_type.to_sym] || flash_type.to_s
   end
   
+  def form_fields(form, prefix, options={})
+    the_function = ->(key, locals={}) do
+      form_field(form, key, locals.merge(options.merge({prefix: prefix})))
+    end
+    yield(the_function)
+    '' # avoid rendering unintended stuff in corner cases
+  end
+  
+  private
+  
+  def form_field form, key, locals={}
+    l = locals.merge(key: key, form: form, as: (locals[:as] || :text))
+    render 'fields/field', l.merge(locals: l)
+  end
+  
 end
