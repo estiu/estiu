477846ee5c440c2e1447ef6e8dcb5e624fc51d0a
vemv <vemv@vemv.net>
2015-10-07 17:28:15 +0200

    Add length validaton

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,8 @@ class Campaign < ActiveRecord::Base
   
   MINIMUM_GOAL_AMOUNT = 100_00
   MAXIMUM_GOAL_AMOUNT = 15_000_00
-  BASIC_ATTRS = %i(name description starts_at ends_at)
-  CREATE_ATTRS = BASIC_ATTRS + [:goal_cents]
+  BASIC_ATTRS = %i(name starts_at ends_at)
+  CREATE_ATTRS = BASIC_ATTRS + [:description, :goal_cents]
   
   belongs_to :event_promoter
   has_and_belongs_to_many :attendees, join_table: :pledges
@@ -14,6 +14,7 @@ class Campaign < ActiveRecord::Base
     validates attr, presence: true
   end
   
+  validates :description, presence: true, length: {minimum: 140}
   validates :goal_cents, presence: true, numericality: {greater_than_or_equal_to: MINIMUM_GOAL_AMOUNT, less_than: MAXIMUM_GOAL_AMOUNT}
   
   attr_accessor :goal_cents_facade
--- spec/factories/campaigns_factory.rb
+++ spec/factories/campaigns_factory.rb
@@ -4,7 +4,7 @@ FG.define do
     
     association :event_promoter
     name "Fund my party"
-    description "Gonna be awesome"
+    description "I was thinking of throwing the perfect party, in a great open-air venue, at daytime, and with some of the latest and greatest DJs out there."
     starts_at { Date.tomorrow }
     ends_at { 30.days.from_now }
     goal_cents { Random.rand(Campaign::MAXIMUM_GOAL_AMOUNT - Campaign::MINIMUM_GOAL_AMOUNT).to_i + Campaign::MINIMUM_GOAL_AMOUNT}
