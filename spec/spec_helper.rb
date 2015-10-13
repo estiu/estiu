ed59098c7891869a017200a3fe2345df246c2862
vemv <vemv@vemv.net>
2015-10-13 02:27:27 +0200

    Move spec helper methods to their own file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'webmock/rspec'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+include SpecHelpers
+
 ActiveRecord::Migration.maintain_test_schema!
 
 WebMock.disable_net_connect!(allow_localhost: true)
@@ -52,57 +54,4 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
-end
-
-def controller_ok status=200
-  expect(response.status).to be status
-  expect(response.body).to be_present
-end
-
-def assign_devise_mapping
-  before {
-    @request.env["devise.mapping"] = Devise.mappings[:user]
-  }
-end
-
-def sign_as role_name=nil, js=false
-  
-  return unless role_name # nil means signed out user
-  
-  let(role_name) { FG.create(:user, "#{role_name}_role".to_sym) }
-  
-  before do
-    user_object = eval(role_name.to_s)
-    user_object.confirm
-    if js
-      login_as(user_object, scope: :user)
-    else
-      sign_in :user, user_object
-    end
-  end
-  
-end
-
-def expect_unauthorized
-  expect(subject).to receive(:user_not_authorized).once.with(any_args).and_call_original
-  expect(subject).to rescue_from(Pundit::NotAuthorizedError).with :user_not_authorized
-end
-
-def forbidden_for *role_names
-
-  role_names.each do |role|
-    
-    context "sign_as #{role || "nil"}" do
-      
-      it 'is forbidden' do
-        
-        expect(response.status).to be 302
-        expect(flash[:alert]).to include(t('application.forbidden'))
-        
-      end
-      
-    end
-    
-  end
-  
 end
