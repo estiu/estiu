72857896d290eaf8d261243bcceb1caaa032a7a9
vemv <vemv@vemv.net>
2015-10-13 03:03:09 +0200

    Leverage `super`

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   
   def sign_up_params
     additional = params.require(:user).permit(attendee_attributes: [:first_name, :last_name]).merge(role: Roles.attendee)
-    devise_parameter_sanitizer.sanitize(:sign_up).merge(additional)
+    super.merge(additional)
   end
   
 end 
