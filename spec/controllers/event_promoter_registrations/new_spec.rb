96b3647a4aeff240940480a76c1043658ef48978
vemv <vemv@vemv.net>
2015-10-14 16:47:39 +0200

    Cover registration controllers :new actions

---
+++ spec/controllers/event_promoter_registrations/new_spec.rb
@@ -0,0 +1,20 @@
+describe EventPromoterRegistrationsController do
+  
+  assign_devise_mapping
+  
+  after do
+    controller_ok 200
+  end
+  
+  describe '#new' do
+      
+    it 'renders the correct form' do
+      
+      get 'new'
+      expect(response).to render_template(partial: 'devise/registrations/_event_promoter')
+      
+    end
+    
+  end
+
+end
---
+++ spec/controllers/registrations/new_spec.rb
@@ -0,0 +1,20 @@
+describe RegistrationsController do
+  
+  assign_devise_mapping
+  
+  after do
+    controller_ok 200
+  end
+  
+  describe '#new' do
+      
+    it 'renders the correct form' do
+      
+      get 'new'
+      expect(response).to render_template(partial: 'devise/registrations/_attendee')
+      
+    end
+    
+  end
+
+end
