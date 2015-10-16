af54bd86581273145e4e2d71384282c6facfa1bb
vemv <vemv@vemv.net>
2015-10-16 16:21:40 +0200

    Correct test - it would mock authentication after visiting the page

--- spec/features/campaigns/show_spec.rb
+++ spec/features/campaigns/show_spec.rb
@@ -2,27 +2,26 @@ describe 'Campaign displaying' do
   
   let(:campaign){ FG.create :campaign }
   
-  before {
-    visit campaign_path(campaign)
-  }
-  
   after {
     page_ok
   }
   
   context 'signed_out' do
+    before { visit campaign_path(campaign) }
     it 'works' do
     end
   end
   
   context 'as an event promoter' do
     sign_as :event_promoter, :feature
+    before { visit campaign_path(campaign) }
     it 'works' do
     end
   end
   
   context 'as an attendee' do
     sign_as :attendee, :feature
+    before { visit campaign_path(campaign) }
     it 'works' do
     end
   end
