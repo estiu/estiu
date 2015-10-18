54499d0578009db110294529933186c2ab0a4118
vemv <vemv@vemv.net>
2015-10-19 01:44:51 +0200

    Define timezone for factory value

--- spec/factories/campaigns_factory.rb
+++ spec/factories/campaigns_factory.rb
@@ -5,7 +5,7 @@ FG.define do
     association :event_promoter
     name "Fund my party"
     description "I was thinking of throwing the perfect party, in a great open-air venue, at daytime, and with some of the latest and greatest DJs out there."
-    starts_at { DateTime.now.end_of_day }
+    starts_at { DateTime.now.end_of_day.utc }
     ends_at { 30.days.from_now }
     goal_cents { Random.rand(Campaign::MAXIMUM_GOAL_AMOUNT - Campaign::MINIMUM_GOAL_AMOUNT).to_i + Campaign::MINIMUM_GOAL_AMOUNT}
     minimum_pledge_cents { 20_00 }
