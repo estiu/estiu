b80c789dd3a9f38d0eb6e65bbb559f573e3da769
vemv <vemv@vemv.net>
2015-10-19 04:23:41 +0200

    Show exceptions in test log

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.static_cache_control = 'public, max-age=3600'
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
-  config.action_dispatch.show_exceptions = false
+  config.action_dispatch.show_exceptions = true
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.delivery_method = :test
   config.active_support.test_order = :random
