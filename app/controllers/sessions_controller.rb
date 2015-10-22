153cd57090171eb30f040d8d9dd0cf74418d1ffd
vemv <vemv@vemv.net>
2015-10-22 19:26:58 +0200

    Correct SessionsController#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < Devise::SessionsController
   
   def new
-    store_location_for(:user, request.referrer) unless params[:controller] == 'sessions'
+    r = request.referrer
+    store_location_for(:user, request.referrer) if r && !r.include?('sign_in')
     super
   end
   
