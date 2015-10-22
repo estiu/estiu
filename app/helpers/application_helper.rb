448b3aaa6ce821c96da6a307cb7bdf5734ecd1cf
vemv <vemv@vemv.net>
2015-10-22 02:15:43 +0200

    Factor out responsive wrappers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,14 @@ module ApplicationHelper
     render 'layouts/progress_bar', percent: percent, label: label, type: type, id: id
   end
   
+  def centered
+    "#{centered_wider} col-lg-4 col-lg-offset-4"
+  end
+  
+  def centered_wider
+    "col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+  end
+  
   private
   
   def form_field form, key, locals={}
