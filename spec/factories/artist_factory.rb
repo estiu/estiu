56bdfc267494382e9d4dd4e773b6aaeb6fd96efe
vemv <vemv@vemv.net>
2015-10-24 00:07:46 +0200

    Fix Artist factory

--- spec/factories/artist_factory.rb
+++ spec/factories/artist_factory.rb
@@ -7,14 +7,18 @@ FG.define do
     email 'dixon@innervisions.com'
     telephone "609929302"
     
+    after(:build) do |rec, eva|
+      
+      unless rec.user
+        rec.user = FG.build :user, artist: rec, roles: [:artist]
+      end
+
+    end
+    
     trait :with_events do
       
       after(:build) do |rec, eva|
-        
-        unless rec.user
-          rec.user = FG.build :user, artist: rec, roles: [:artist]
-        end
-
+      
         rec.events << FactoryGirl.build(:event, artists: [rec])
         
       end
