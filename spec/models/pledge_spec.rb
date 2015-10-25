7dc93fd73e6609c24e1ddf77f4027c26a9e60b5e
vemv <vemv@vemv.net>
2015-10-26 00:02:59 +0100

    Cover new Pledge methods

--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -73,4 +73,90 @@ describe Pledge do
     
   end
   
+  describe '#calculate_discount!' do
+    
+    let(:campaign) { FG.create :campaign, :almost_fulfilled }
+    
+    subject{ FG.build :pledge, campaign: campaign, referral_email: email }
+    
+    before {
+      expect(subject.discount_cents).to be 0
+    }
+    
+    def the_test negative=false
+      if negative
+        expect {
+          subject.calculate_discount!
+        }.to_not change {
+          subject.discount_cents
+        }
+      else
+        expect {
+          subject.calculate_discount!
+        }.to change {
+          subject.discount_cents
+        }.from(0).to(Pledge::DISCOUNT_PER_REFERRAL)
+      end
+    end
+    
+    context 'related email' do
+      
+      let(:email){ campaign.attendees.first.user.email }
+      
+      it 'sets a discount' do
+        the_test
+      end
+      
+    end
+    
+    context 'unrelated email' do
+    
+      let(:email){ "#{SecureRandom.hex}@#{SecureRandom.hex}.com" }
+      
+      it 'does not set a discount' do
+        the_test :negative
+      end
+      
+    end
+    
+    context 'non-email string' do
+    
+      let(:email) { SecureRandom.hex }
+      
+      it 'does not set a discount' do
+        the_test :negative
+      end
+      
+    end
+    
+    context 'blank email' do
+    
+      let(:email){ nil }
+      
+      it 'does not set a discount' do
+        the_test :negative
+      end
+      
+    end
+    
+  end
+  
+  describe '#calculate_total!' do
+    
+    before {
+      subject.amount_cents = nil
+      expect(subject.originally_pledged_cents).to_not be nil
+      expect(subject).to receive(:calculate_discount!).and_call_original
+    }
+    
+    it 'sets amount_cents' do
+      expect {
+        subject.calculate_total!
+      }.to change {
+        subject.amount_cents
+      }.from(nil).to(subject.originally_pledged_cents)
+    end
+    
+  end
+  
 end
