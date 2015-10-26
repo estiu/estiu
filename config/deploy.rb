8be332701357bbe0f272a9612f4e445e2ee1d6d9
vemv <vemv@vemv.net>
2015-10-26 21:18:37 +0100

    Change cap log level and target server

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,14 +10,10 @@ set :repo_url, 'git@bitbucket.org:vemv/events.git'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '~/events'
 
-# Default value for :scm is :git
-# set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
-# Default value for :log_level is :debug
-set :log_level, :info
+set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
