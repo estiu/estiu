c5e28710e1daa0f546b00f75702b82345b077b27
vemv <vemv@vemv.net>
2015-10-29 04:35:03 +0100

    Update AwsOps

--- app/lib/aws_ops.rb
+++ app/lib/aws_ops.rb
@@ -2,13 +2,15 @@ class AwsOps
   
   def self.security_groups
     {
-      port_80: "sg-b39522d7"
+      port_80_public: "sg-e19e2985",
+      port_80_vpc: "sg-fd9e2999"
     }
   end
   
+  VPC = '172.32.0.0/16'
   ELB_NAME = 'ELB'
   ASG_NAME = 'ASG'
-  LAUNCH_CONFIGURATION_NAME = "webCopy"
+  LAUNCH_CONFIGURATION_NAME = "web"
   AVAILABILITY_ZONES = ['eu-west-1a', 'eu-west-1b', 'eu-west-1c']
   
   def self.create_elb
@@ -20,10 +22,10 @@ class AwsOps
           protocol: "HTTP",
           load_balancer_port: 80,
           instance_protocol: "HTTP",
-          instance_port: 3000
+          instance_port: 80
         }
       ],
-      security_groups: [security_groups[:port_80]],
+      security_groups: [security_groups[:port_80_public]],
       availability_zones: AVAILABILITY_ZONES
     })
   end
@@ -38,6 +40,15 @@ class AwsOps
       load_balancer_names: [ELB_NAME],
       availability_zones: AVAILABILITY_ZONES
     })
+  end
+  
+  def self.create!
+    begin
+      create_elb && create_asg
+    rescue Exception => e
+      # TODO - destroy partially created resources (e.g. elb succeeds, asg fails.)
+      raise e
+    end
   end
   
 end
