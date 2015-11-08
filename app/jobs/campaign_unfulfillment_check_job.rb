ada688dfae98feb43c70b32b3878e879a986f6eb
vemv <vemv@vemv.net>
2015-11-08 07:13:21 +0100

    Only invoke DataPipeline in production

--- app/jobs/campaign_unfulfillment_check_job.rb
+++ app/jobs/campaign_unfulfillment_check_job.rb
@@ -1,6 +1,7 @@
 class CampaignUnfulfillmentCheckJob < ActiveJob::Base
   
   def perform campaign_id
+    return unless Rails.env.production?
     AwsOps::Pipeline.schedule_campaign_unfulfillment_check(Campaign.find(campaign_id))
   end
   
--- app/jobs/campaign_unfulfillment_unschedule_job.rb
+++ app/jobs/campaign_unfulfillment_unschedule_job.rb
@@ -1,6 +1,7 @@
 class CampaignUnfulfillmentUnscheduleJob < ActiveJob::Base
   
   def perform campaign_id
+    return unless Rails.env.production?
     AwsOps::Pipeline.unschedule_campaign_unfulfillment_check campaign_id
   end
   
