04f73468f6d64251175b7d168827ed95830c4af4
vemv <vemv@vemv.net>
2015-11-08 16:27:34 +0100

    Remove events_for_bundling mechanism

--- packer/base/build.sh
+++ packer/base/build.sh
@@ -11,7 +11,6 @@ sudo apt-get -qq -y update
 sudo apt-get -qq -y install git zsh build-essential libssl-dev libreadline-dev zlib1g-dev libpq-dev tree nginx
 echo -e "Host bitbucket.org\nIdentityFile ~/.ssh/deployment_key\nStrictHostKeyChecking no\nUserKnownHostsFile=/dev/null" >> .ssh/config
 chmod 0600 ~/.ssh/deployment_key*
-git clone git@bitbucket.org:vemv/events.git events_test
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
 echo 'eval "$(rbenv init -)"' >> ~/.profile
@@ -20,8 +19,6 @@ source ~/.profile
 rbenv install 2.2.3
 rbenv global 2.2.3
 gem install bundler
-cd ~/events_clone_for_bundling
-export GEM_HOME=
-bundle --without development test
-cd
-rm -rf ~/events_clone_for_bundling
+git clone git@bitbucket.org:vemv/events.git events_test
+cd events_test
+bundle --without development test --path vendor/bundle
