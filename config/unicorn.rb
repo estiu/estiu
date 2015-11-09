c00273f139b88c54cc26697550f3f8f6151c53e0
vemv <vemv@vemv.net>
2015-11-09 06:55:19 +0100

    Remove references to Capistrano directories

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,10 @@
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/../../shared/"
 dev_env = !File.exists?(File.join(app_dir, 'RAILS_ENV'))
-log_path = (dev_env ? "#{app_dir}/" : shared_dir) + "log"
-pid_file = (dev_env ? "#{app_dir}/tmp/" : shared_dir) + 'pids/unicorn.pid'
+capistrano = false
+use_default_directories = dev_env || !capistrano
+log_path = (use_default_directories ? "#{app_dir}/" : shared_dir) + "log"
+pid_file = (use_default_directories ? "#{app_dir}/tmp/" : shared_dir) + 'pids/unicorn.pid'
 
 working_directory app_dir
 worker_processes (dev_env ? 1 : 2)
