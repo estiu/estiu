eed953a30dc721ad1932b56ff4b33a65b2c07b66
vemv <vemv@vemv.net>
2015-11-09 11:59:22 +0100

    Don't start Unicorn or Shoryuken at startup, since .env files will not be available at every time

--- packer/web/build.sh
+++ packer/web/build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash -x
 
-# Codeship sets this value, which shouldn't exist.
-unset GEM_HOME
-
 set -e
 set -u
 sleep 30
+source ~/.profile
 cd
-sudo mv files/unicorn_events /etc/init.d
-sudo chmod 755 /etc/init.d/unicorn_events
-sudo update-rc.d unicorn_events defaults
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo mv files/nginx_events /etc/nginx/sites-enabled/default
 sudo service nginx restart
 rm -rf files
 cd events
 git pull
+bundle
 cd -
--- packer/worker/build.sh
+++ packer/worker/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -x
 
-# Codeship sets this value, which shouldn't exist.
-unset GEM_HOME
-
 set -e
 set -u
 sleep 30
+source ~/.profile
 cd
 sudo update-rc.d -f nginx remove
-sudo chmod 755 files/shoryuken.conf
-sudo mv files/shoryuken.conf /etc/init/
 cd events
 git pull
+bundle
 cd -
