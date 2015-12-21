e8fd0da04e2e73a2128df245d9e7b24f5c80c148
vemv <vemv@vemv.net>
2015-12-21 02:17:48 +0100

    Separate Credit creation mails depending on the type

--- app/mailer_previews/credit_creation_preview.rb
+++ app/mailer_previews/credit_creation_preview.rb
@@ -1,7 +1,19 @@
 class CreditCreationPreview < ActionMailer::Preview
   
-  def perform
-    CreditCreationMailer.perform(Credit.unscoped.first)
+  def referral_pledge
+    credit = Credit.where.not(referral_pledge_id: nil).first
+    unless credit
+      credit = FG.create :credit, :referral
+    end
+    CreditCreationMailer.perform credit
+  end
+  
+  def refunded_pledge
+    credit = Credit.where.not(refunded_pledge_id: nil).first
+    unless credit
+      credit = FG.create :credit, :refund
+    end
+    CreditCreationMailer.perform credit
   end
   
 end
--- app/mailers/credit_creation_mailer.rb
+++ app/mailers/credit_creation_mailer.rb
@@ -3,7 +3,14 @@ class CreditCreationMailer < ApplicationMailer
   def perform credit
     @credit = credit
     @amount = credit.amount.format
-    @referrer = credit.referral_pledge.attendee.first_name
+    @type = credit.referral_pledge ? :referral_pledge : credit.refunded_pledge ? :refunded_pledge : raise
+    if credit.referral_pledge
+      @referrer = credit.referral_pledge.attendee.first_name
+    elsif credit.refunded_pledge
+      # nothing
+    else
+      raise
+    end
     mail(to: credit.attendee.user.email, subject: t(".subject", amount: @amount))
   end
   
