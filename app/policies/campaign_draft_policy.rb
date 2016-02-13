666248d50323e0ec9b580f4a0013e554db4c3e92
vemv <vemv@vemv.net>
2016-02-13 10:39:28 +0100

    Implement CampaignDraft approval/rejection UI

--- app/policies/campaign_draft_policy.rb
+++ app/policies/campaign_draft_policy.rb
@@ -24,4 +24,10 @@ class CampaignDraftPolicy < ApplicationPolicy
     edit_or_update?
   end
   
+  %i(approve? reject?).each do |method|
+    define_method method do
+      user.admin? && record.must_be_reviewed?
+    end
+  end
+  
 end
