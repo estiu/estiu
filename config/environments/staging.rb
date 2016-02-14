9aac3bc4660fce2e8302c62ea5b98a22caa3e119
vemv <vemv@vemv.net>
2016-02-14 06:42:32 +0100

    Run jobs inline if staging environment is being run from a developer machine

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,5 @@ require_relative 'production_or_staging'
 
 Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
+  config.active_job.queue_adapter = DeveloperMachine.running_in_developer_machine? ? :inline : :shoryuken
 end
