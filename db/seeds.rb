45c2330a19f1b80ec81af167cbe05a2ddbff9fb3
vemv <vemv@vemv.net>
2016-04-13 01:59:19 +0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 if Rails.env.development?
-  user = FG.create :user, roles: Roles.all, email: 'vemv91@gmail.com', password: 'p'
   4.times { FG.create :venue }
+  user = User.find_by_email('vemv91@gmail.com') || FG.create(:user, roles: Roles.all, email: 'vemv91@gmail.com', password: 'p')
   FG.create :campaign
   FG.create :campaign, :almost_fulfilled
   2.times { FG.create :campaign, :unfulfilled, including_attendees: [user.attendee] }
-  2.times { FG.create :campaign, :fulfilled, campaign_draft: FG.create(:campaign_draft, :published, event_promoter_id: user.event_promoter_id) }
+  2.times { FG.create :campaign, :fulfilled, campaign_draft: FG.build(:campaign_draft, :published, event_promoter_id: user.event_promoter_id) }
   FG.create :campaign, :with_referred_attendee, referred_attendee: user.attendee
   FG.create :event, event_promoter_id: user.event_promoter_id
   !user.attendee.credits.count.zero? || fail
