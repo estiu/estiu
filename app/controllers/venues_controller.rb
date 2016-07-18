bb9240e2ba6c6282f214392e12fe1ac8b90b08d0
vemv <vemv@vemv.net>
2016-07-18 23:33:04 +0200

    Generate the correct input names for venue selects (CampaignDraft prefix was being used for Events)

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,9 +2,9 @@ class VenuesController < ApplicationController
   
   def create
     authorize Venue
-    venue = Venue.new venue_params
+    venue = Venue.new venue_params.except(OBJECT_FOR_FORM_KEY)
     if venue.save
-      render 'venues/create_successful', locals: {selected_id: venue.id}, layout: false, status: 200
+      render 'venues/create_successful', locals: {selected_id: venue.id, object_for_form: object_for_form}, layout: false, status: 200
     else
       flash.now[:error] = t '.error'
       render 'layouts/flash_messages_js', layout: false, status: 422
@@ -13,8 +13,15 @@ class VenuesController < ApplicationController
   
   protected
   
+  OBJECT_FOR_FORM_KEY = 'object_for_form'
+  OBJECT_FOR_FORM_HASH = Hash.new{ raise }.merge("event" => Event.new, "campaign_draft" => CampaignDraft.new).freeze
+  
   def venue_params
-    params.require(:venue).permit(Venue::CREATE_ATTRS)
+    params.require(:venue).permit(Venue::CREATE_ATTRS + [OBJECT_FOR_FORM_KEY])
+  end
+  
+  def object_for_form
+    OBJECT_FOR_FORM_HASH[venue_params.require(OBJECT_FOR_FORM_KEY)]
   end
   
 end
