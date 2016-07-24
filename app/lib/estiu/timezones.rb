d62a5396946067f060e191b59b9af84627c0141d
vemv <vemv@vemv.net>
2016-07-24 11:10:19 +0200

    Formatting

--- app/lib/estiu/timezones.rb
+++ app/lib/estiu/timezones.rb
@@ -25,7 +25,16 @@ class Estiu::Timezones
     "Mountain Time (US & Canada)",
     "Pacific Time (US & Canada)"
   ]
-  FOR_SELECT = ALL_TIMEZONE_OBJECTS.map{|tz| [tz.to_s, tz.tzinfo.name] }.uniq.sort.group_by{|a| a[0][0..10] }.map{|k, v| [k, v[0].second, v.map{|a|  a.first }.map{|a| a[12..(a.length - 1)]}.select{|a| PREFERRED_CITIES.include?(a)} ] }.sort_by{|a| [(a[2].blank? ? "b" : "a"), a[0]] }.map{|a| a[2] = [a[0]] if a[2].blank?; [a[2].sort.join(', '), a[1]] }
+  FOR_SELECT = ALL_TIMEZONE_OBJECTS.
+    map{|tz| [tz.to_s, tz.tzinfo.name] }.
+    uniq.
+    sort.
+    group_by{|a| a[0][0..10] }.
+    map{|k, v| [k, v[0].second, v.map{|a|  a.first }.
+    map{|a| a[12..(a.length - 1)]}.
+    select{|a| PREFERRED_CITIES.include?(a)} ] }.
+    sort_by{|a| [(a[2].blank? ? "b" : "a"), a[0]] }.
+    map{|a| a[2] = [a[0]] if a[2].blank?; [a[2].sort.join(', '), a[1]] }
   
 end
 
