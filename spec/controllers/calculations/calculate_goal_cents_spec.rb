2dd526311620c8b4cde62c13c33370c29b20a4c5
vemv <vemv@vemv.net>
2016-07-30 12:38:40 +0200

    Cover CalculationsController#calculate_goal_cents

---
+++ spec/controllers/calculations/calculate_goal_cents_spec.rb
@@ -0,0 +1,84 @@
+describe CalculationsController do
+  
+  describe '#calculate_goal_cents' do
+    
+    def the_result
+      JSON.parse(response.body).fetch 'value'
+    end
+    
+    describe 'permitted roles' do
+      
+      after do
+        controller_ok
+      end
+      
+      def the_tests
+        
+        get :calculate_goal_cents, proposed_goal_cents: 0
+        expect(the_result).to be_present
+        expect(Monetize.parse(the_result).to_i).to eq 0
+        
+        # ---
+        
+        get :calculate_goal_cents, proposed_goal_cents: 1
+        expect(the_result).to be_present
+        
+        # ---
+        
+        [1_00, 100_00, 1_000_00, 10_000_00, 100_000_00, 100_000_000_00].each do |proposed_goal_cents|
+          
+          get :calculate_goal_cents, proposed_goal_cents: proposed_goal_cents
+          expect(the_result).to be_present
+          expect(Monetize.parse(the_result)).to be > Money.new(proposed_goal_cents)
+          
+        end
+        
+        # ---
+        
+        proposed_goal_cents = 1_000_00
+        goal = Money.new(9_123_456_78)
+        campaign = double(generate_goal_cents: nil, goal: goal)
+        expect(CampaignDraft).to receive(:new).with({proposed_goal_cents: proposed_goal_cents.to_s}).and_return(campaign)
+        get :calculate_goal_cents, proposed_goal_cents: proposed_goal_cents
+        expect(Monetize.parse(the_result).to_i).to eq goal.to_i
+        
+      end
+      
+      context 'event_promoter role' do
+        
+        sign_as :event_promoter
+        
+        it "works" do
+          the_tests
+        end
+        
+      end
+      
+      context 'admin role' do
+        
+        sign_as :admin
+        
+        it "works" do
+          the_tests
+        end
+        
+      end
+      
+    end
+    
+    
+    context 'forbidden roles' do
+      
+      before {
+        expect_unauthorized
+        get :calculate_goal_cents, proposed_goal_cents: 1
+      }
+      
+      forbidden_for(nil, :attendee)
+      
+    end
+      
+    
+  end
+  
+end
