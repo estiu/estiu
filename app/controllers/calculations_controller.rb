0c3df4501b75933aa5893377a1d303a254893c0f
vemv <vemv@vemv.net>
2016-07-30 04:16:59 +0200

    Present currency values more correctly

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,12 +1,14 @@
 class CalculationsController < ApplicationController
   
+  FORMAT_OPTS = {no_cents_if_whole: false}.freeze
+  
   before_action :authorize_calculation
   
   # applies taxes and commissions (taxes not implemented yet).
   def calculate_goal_cents
     campaign = CampaignDraft.new(proposed_goal_cents: params.require(:proposed_goal_cents))
     campaign.generate_goal_cents
-    value = campaign.goal.format
+    value = campaign.goal.format(FORMAT_OPTS.dup)
     render json: {value: value}
   end
   
